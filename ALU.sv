0937a270d205a00db06ee40e4e643f5c0bcc0aaf
99angelrm <99angelrm@gmail.com>
2021-09-18 23:39:37 -0500

    Changed SLT with sign

--- ALU.sv
+++ ALU.sv
@@ -9,13 +9,13 @@ module ALU(
 
 
 always_comb alu_result = (control_input==0) ? read_data_1 & mux_alu_src :
-								 (control_input==1) ? read_data_1 | mux_alu_src :
-								 (control_input==2) ? read_data_1 + mux_alu_src :
-								 (control_input==6) ? read_data_1 - mux_alu_src :
-								 (control_input==7) & (read_data_1 <= mux_alu_src) ? 32'h00000001:
-								 (control_input==7) & ~(read_data_1 <= mux_alu_src) ? 32'h00000000:
-								 (control_input==12) ? ~(read_data_1 | mux_alu_src) :
-								 1'bx;
+						 (control_input==1) ? read_data_1 | mux_alu_src :
+						 (control_input==2) ? read_data_1 + mux_alu_src :
+						 (control_input==6) ? read_data_1 - mux_alu_src :
+						 (control_input==7) & ($signed(read_data_1) < $signed(mux_alu_src)) ? 32'h00000001:
+						 (control_input==7) & ~($signed(read_data_1) < $signed(mux_alu_src)) ? 32'h00000000:
+						 (control_input==12) ? ~(read_data_1 | mux_alu_src) :
+						 1'bx;
 								 
 always_comb zero = (alu_result==0) ? 1'b1:1'b0;
 								 
--- MIPS_tb.sv
+++ MIPS_tb.sv
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+// `timescale 1ns / 1ps
 module MIPS_tb;
 
   logic 		clk;
@@ -22,7 +22,7 @@ MIPS uut(
 		rst = 1;
 
 		// Wait 100 ns for global reset to finish
-		#11;
+		#10;
       		rst = 0; 
 		
 		// INIT 
@@ -30,16 +30,53 @@ MIPS uut(
 			//uut.dpt.registros.mem[i] = i;
 			//end
 		for ( i=0 ; i<10 ;i = i + 1)begin
-			uut.registers.mem[i] = i;
+			uut.registers.mem[i] = '0;
 		end
 		
 		for ( i=0 ; i<256 ;i = i + 1) begin
 			randdata = $random();
-			uut.datamem.mem_array[i] = {8'h0,randdata[7:0]};  
+			uut.datamem.mem_array[i] = {8'h0,randdata[7:0]};
+			uut.datamem.mem_array[i+256] = uut.datamem.mem_array[i];
 			end	
 
-uut.instructionmem.mem_array[0] = 32'b00100000010000010000000000000011;
+			uut.datamem.mem_array[1] = 100;
+			uut.datamem.mem_array[1+256] = 60;
+			
+			uut.datamem.mem_array[2] = 40;
+			uut.datamem.mem_array[2+256] = 100;
+			
+			uut.datamem.mem_array[3] = -2;
+			uut.datamem.mem_array[3+256] = 2;
+			
+			uut.datamem.mem_array[4] = 40;
+			uut.datamem.mem_array[4+256] = 20;
+			
+			uut.datamem.mem_array[5] = 60;
+			uut.datamem.mem_array[5+256] = 0;
+			
+			uut.datamem.mem_array[6] = 0;
+			uut.datamem.mem_array[6+256] = -1;
+			
+			uut.datamem.mem_array[7] = -50;
+			uut.datamem.mem_array[7+256] = -25;
+			
+			uut.datamem.mem_array[8] = 50;
+			uut.datamem.mem_array[8+256] = -25;
+			
+			uut.datamem.mem_array[9] = -69;
+			uut.datamem.mem_array[9+256] = -0;
 
+			uut.instructionmem.mem_array[1]  = 32'b001000_00110_00110_0000000000000001;
+			uut.instructionmem.mem_array[2]  = 32'b100011_00001_00010_0000000000000000;
+			uut.instructionmem.mem_array[3]  = 32'b100011_00001_00011_0000000100000000;
+			uut.instructionmem.mem_array[4]  = 32'b000000_00010_00011_0010000000100010;
+			uut.instructionmem.mem_array[5]  = 32'b000000_00100_00000_0010100000101010;
+			uut.instructionmem.mem_array[6]  = 32'b000100_00110_00101_0000000000000100;
+			uut.instructionmem.mem_array[7]  = 32'b000000_00111_00100_0011100000100000;
+			uut.instructionmem.mem_array[8]  = 32'b001000_00001_00001_0000000000000001;
+			uut.instructionmem.mem_array[9]  = 32'b000010_00000_00000_0000000000000010;
+			uut.instructionmem.mem_array[10] = 32'b000000_00000_00100_0010000000100010;
+			uut.instructionmem.mem_array[11] = 32'b000010_00000_00000_0000000000000111;
 
 		
 end
